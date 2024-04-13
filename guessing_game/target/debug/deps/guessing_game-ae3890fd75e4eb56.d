8d2a5ab14186b8a36f3a5bfccbfcf94202097304
Felipe Arce <arceprogramando@gmail.com>
2024-04-13 15:20:37 -0300

    fix(backend): :fire: remove cargo target

--- guessing_game/target/debug/deps/guessing_game-ae3890fd75e4eb56.d
+++
@@ -1,5 +0,0 @@
-/home/felipe/rust/guessing_game/target/debug/deps/guessing_game-ae3890fd75e4eb56: src/main.rs
-
-/home/felipe/rust/guessing_game/target/debug/deps/guessing_game-ae3890fd75e4eb56.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/felipe/rust/guessing_game/target/debug/guessing_game: /home/felipe/rust/guessing_game/src/main.rs
