17d422bc11639b2dec058d44bdaf8bfa2a3f2d49
Felipe Arce <arceprogramando@gmail.com>
2025-04-29 23:01:32 -0300

    feat: refactor minigrep

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("No hay suficientes argumentos");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+       Ok( Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,32 @@
 use std::env;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
     
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
+
+    println!("Title: {}",config.query);
+    println!("In the file: {}",config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
+
+
+
+
+/*fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+   Config {query, file_path}
+}
+   */
